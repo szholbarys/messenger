0319ea84f71e9c10ae385801374cd4439b159b78
szholbarys <sakenzholbarys@gmail.com>
2023-11-21 21:02:20 +0600

    siuuu

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  transpilePackages: ['typing-indicator'],
+  assetPrefix: './',
+  basePath: '/nextjs-github-pages'
+}
+
+module.exports = nextConfig
